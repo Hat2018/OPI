1ad6ca8a191c3a25f417d8217e9a9daa62c89d4b
Hat2018 <45607852+Hat2018@users.noreply.github.com>
2018-12-04 23:23:57 +0200

    Add files via upload

---
+++ client-server/client/client/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace client
+{
+    
+        class Program
+        {
+            static int port = 8005; // порт сервера
+            static string address = "127.0.0.1"; // адрес сервера
+            static void Main(string[] args)
+            {
+                try
+                {
+                    IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+                //1 create socket 
+                    Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    //2 подключение method connect socket  +  send 
+                    socket.Connect(ipPoint);
+                    Console.Write("Введите сообщение:");
+                    string message = Console.ReadLine();
+                    byte[] data = Encoding.Unicode.GetBytes(message);
+                    socket.Send(data);
+
+                    //3 получаем ответ  method receive
+                    data = new byte[256]; // буфер для ответа
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0; // количество полученных байт
+
+                    do
+                    {
+                        bytes = socket.Receive(data, data.Length, 0);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (socket.Available > 0);
+                    Console.WriteLine("Ответ: " + builder.ToString());
+
+                    //4 закрываем сокет method close
+                    socket.Shutdown(SocketShutdown.Both);
+                    socket.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                Console.Read();
+            }
+        }
+    
+}
---
+++ client-server/server/server/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+
+namespace server
+{
+
+    class Program
+    {
+
+        static string path = "D:\\";
+        static int port = 8005; 
+        static string address = "127.0.0.1";
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            //адрес для запуска сокета
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+
+            //1. создаем сокет
+            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                //2. method Bind связываем сокет с локальной точкой, по которой будем принимать данные
+                listenSocket.Bind(ipPoint);
+
+                //3. method Listen начинаем прослушивание
+                listenSocket.Listen(10);
+
+                Console.WriteLine("Сервер запущен. Подключение...");
+
+                while (true)
+                    //4. method Accept получение входящего подключения  
+                {
+                    Socket handler = listenSocket.Accept();
+                    // получаем сообщение
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0; // количество полученных байтов
+                    byte[] data = new byte[256]; // буфер для получаемых данных
+                    int sizebuf = handler.Receive(data);
+                    string decode = "";
+                    for (int i = 0; i < sizebuf; i++)
+                    {
+                        decode += Convert.ToChar(data[i]);
+                    }
+                    Console.WriteLine(decode);
+                    
+                    while (handler.Available > 0);
+                    //D
+                     {
+                     
+                            if (Directory.Exists(path))
+                            {
+                                Console.WriteLine("Корневая папка:");
+                                string[] dir = Directory.GetDirectories(path);
+                                foreach (string str in dir)
+                                {
+                                    Console.WriteLine(str);
+                                }
+                                Console.WriteLine("Файлы: ");
+                                string[] file = Directory.GetFiles(path);
+                                foreach (string str in file)
+                                {
+                                    Console.WriteLine(str);
+                                }
+                            }
+                       
+                     }
+                       //5. method Send/receive + 6. method Close socket             
+                    string message = "Cообщение доставлено";
+                    data = Encoding.Unicode.GetBytes(message);
+                    handler.Send(data);
+                    handler.Shutdown(SocketShutdown.Both);// закрываем сокет
+                    handler.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+    }
+}  
